df6a40e37dfd626c7bf82ad8bfa79281df5f9ef4
boddulavineela <boddulavineela@gmail.com>
2015-11-12 02:21:35 -0500

    Added Shell scripts

---
+++ build-vm/shell/InstallBoxStarter.bat
@@ -0,0 +1,5 @@
+chocolatey feature enable -n=allowGlobalConfirmation
+
+choco install vcredist2005
+
+chocolatey feature disable -n=allowGlobalConfirmation
---
+++ build-vm/shell/install-MCR.cmd
@@ -0,0 +1,5 @@
+@echo off
+
+echo "Installing MCR; it will take a while..."
+START /WAIT c:\vagrant\software\mcrinstaller.exe /w /s /v/qn
+echo "Done installing MCR."
