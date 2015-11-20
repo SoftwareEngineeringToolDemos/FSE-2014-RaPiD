78b6d31afdfdeb2db62678e5abf58f6c55db953f
Vineela Boddula <boddulavineela@gmail.com>
2015-11-19 20:37:42 -0500

    Update install-MCR.cmd
    Included command to download the Matlab installer from the website

--- build-vm/shell/install-MCR.cmd
+++ build-vm/shell/install-MCR.cmd
@@ -1,5 +1,9 @@
 @echo off
 
+echo "Downloading MCRInstaller...."
+powershell -command "& { (new-object System.Net.WebClient).DownloadFile('https://profilelingui.files.wordpress.com/2015/08/mcrinstaller-exe.pdf','c:\vagrant\software\mcrinstaller.exe') }"
 echo "Installing MCR; it will take a while..."
+
 START /WAIT c:\vagrant\software\mcrinstaller.exe /w /s /v/qn
+
 echo "Done installing MCR."
