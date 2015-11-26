c9dc0553de471c33cbc56b3b5f2e3221bcd1d168
Vineela <boddulavineela@gmail.com>
2015-11-26 09:29:58 -0500

    Updated Scripts

--- build-vm/Scripts/install-dotnet.bat
+++ build-vm/Scripts/install-dotnet.bat
@@ -1,5 +1,6 @@
 chocolatey feature enable -n=allowGlobalConfirmation
 
 choco install vcredist2005
+choco install dotnet4.0 -version 4.0.30319
 
 chocolatey feature disable -n=allowGlobalConfirmation
