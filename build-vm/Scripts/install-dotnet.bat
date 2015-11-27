980a7118e5f4b2a36fc9ab6a92149ccf7ad03469
Vineela Boddula <boddulavineela@gmail.com>
2015-11-27 15:07:44 -0500

    install vc ++ 2005 file

--- build-vm/Scripts/install-dotnet.bat
+++ build-vm/Scripts/install-dotnet.bat
@@ -1,6 +1,5 @@
 chocolatey feature enable -n=allowGlobalConfirmation
 
 choco install vcredist2005
-choco install dotnet4.0 -version 4.0.30319
 
 chocolatey feature disable -n=allowGlobalConfirmation
